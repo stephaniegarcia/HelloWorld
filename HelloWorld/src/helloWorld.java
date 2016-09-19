348eab2f56d1aba3afa53e200a18e7f6f7b01efa
ic070_6 <ic070_6@anon.ece.uprm.edu>
2016-09-19 15:19:20 -0400

    Updated

--- HelloWorld/src/helloWorld.java
+++ HelloWorld/src/helloWorld.java
@@ -4,6 +4,8 @@ public class helloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("This sentence is for testing purposes.");
 	}
 
 }
